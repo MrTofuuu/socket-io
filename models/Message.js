7ef2b9a57409bc5339f669fbfa59ac4afb9fc882
MrTofuuu <chris.stallcup@gmail.com>
2024-06-26 00:19:10 -0500

    initial POC with working client socket event emitted to server

---
+++ models/Message.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Message extends Model {}
+
+Message.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    client_offset: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    content: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'message',
+  }
+);
+
+module.exports = Message;
--- server.js
+++ server.js
@@ -1,47 +1,67 @@
 require('dotenv').config()
 const path = require('path');
 const express = require('express');
+const { createServer } = require('node:http');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const { Server } = require('socket.io');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// async function main(){
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+  //Socket IO 
+  // const io = new Server()
+  const app = express();
 
-const sess = {
-  secret: process.env.SESS_SECRET,
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
+  const PORT = process.env.PORT || 3001;
+  
+  // Set up Handlebars.js engine with custom helpers
+  const hbs = exphbs.create({ helpers });
+  
+  const sess = {
+    secret: process.env.SESS_SECRET,
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+  
+  app.use(session(sess));
+  
+  // Inform Express.js on which template engine to use
+  app.engine('handlebars', hbs.engine);
+  app.set('view engine', 'handlebars');
+  
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  
+  app.use(routes);
+  
+  // creating new http server 
+  const httpServer = createServer(app);
+  const io = new Server(httpServer);
+  io.on("connection", (socket)=>{
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
   })
-};
+  sequelize.sync({ force: false }).then(() => {
+    // app.listen(PORT, () => console.log('Now listening'));
+    httpServer.listen(PORT,() => console.log(`Now listening at http://localhost:${PORT}`))  
+  });
+  
+// }
 
-app.use(session(sess));
-
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// main();
